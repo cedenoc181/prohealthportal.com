b0928242f4f8122491aa073fdef7da9d11499827
Christian <cedenoc181@gmail.com>
2024-09-09 18:43:36 -0400

    my template

--- db/migrate/20240909214401_create_my_templates.rb
+++ db/migrate/20240909214401_create_my_templates.rb
@@ -1,7 +1,10 @@
 class CreateMyTemplates < ActiveRecord::Migration[7.1]
   def change
     create_table :my_templates do |t|
-
+      t.references :user_id
+      t.references :patient_template_id
+      t.references :dr_template_id
+      t.text :notes
       t.timestamps
     end
   end
