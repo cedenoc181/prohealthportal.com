9f847dc4798837f66a6a837afd6a5f7ca8948d35
Christian <cedenoc181@gmail.com>
2024-09-09 18:26:37 -0400

    my medifiles migration

--- db/migrate/20240909214309_create_my_medifiles.rb
+++ db/migrate/20240909214309_create_my_medifiles.rb
@@ -1,7 +1,11 @@
 class CreateMyMedifiles < ActiveRecord::Migration[7.1]
   def change
     create_table :my_medifiles do |t|
-
+      t.references :user, foreign_key: true
+      t.references :medifile, foreign_key: true
+      t.string :my_file_title
+      t.text :my_file_description
+      t.string :provider_name, default: "no provider associated"
       t.timestamps
     end
   end
