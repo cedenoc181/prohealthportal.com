69dbf29d071b33089718e21c5bff1a074ef7cab9
Christian <cedenoc181@gmail.com>
2024-09-09 17:56:22 -0400

    user migration

--- db/migrate/20240909213752_create_users.rb
+++ db/migrate/20240909213752_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-
+      t.string :full_name
+      t.string :email
+      t.string :password_digest
+      t.string :role
+      t.string :clinic_location
+      t.string :insurance_network, default: "Not Provided"
+      t.boolean :direct_access
+      t.boolean :admin, default: false
       t.timestamps
     end
   end
