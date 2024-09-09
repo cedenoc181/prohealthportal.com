923ee50b3b95b92c7bc4bba7bc19dc3444738860
Christian <cedenoc181@gmail.com>
2024-09-09 18:39:13 -0400

    dr template migration

--- db/migrate/20240909214335_create_patient_templates.rb
+++ db/migrate/20240909214335_create_patient_templates.rb
@@ -3,7 +3,7 @@ class CreatePatientTemplates < ActiveRecord::Migration[7.1]
     create_table :patient_templates do |t|
       t.string :px_temp_title
       t.text :px_temp_content
-      t.string :category #retention, follow up, inquiry, HEP, B-dasy etc.
+      t.string :category #retention, follow up, inquiry, HEP, B-day, review-acknowledgement etc.
       t.string :language, default: "english" #spanish or english
       t.timestamps
     end
--- db/migrate/20240909214348_create_dr_templates.rb
+++ db/migrate/20240909214348_create_dr_templates.rb
@@ -1,7 +1,9 @@
 class CreateDrTemplates < ActiveRecord::Migration[7.1]
   def change
     create_table :dr_templates do |t|
-
+      t.string :dr_temp_title
+      t.text :dr_temp_content
+      t.string :category #POC, follow up, referral, APOS-letter, etc.
       t.timestamps
     end
   end
