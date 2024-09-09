2d77a6b71cd58d11a21adf4be5ba46196c9289e7
Christian <cedenoc181@gmail.com>
2024-09-09 18:05:59 -0400

    medifiles migration

--- db/migrate/20240909214234_create_medifiles.rb
+++ db/migrate/20240909214234_create_medifiles.rb
@@ -1,7 +1,14 @@
 class CreateMedifiles < ActiveRecord::Migration[7.1]
   def change
     create_table :medifiles do |t|
-
+      t.string :title
+      t.text :description
+      t.string :file_link
+      t.string :file_cover
+      t.string :file_cover_alt
+      t.date :publish_date
+      t.string :language
+      t.boolean :file_editable, default: false
       t.timestamps
     end
   end
