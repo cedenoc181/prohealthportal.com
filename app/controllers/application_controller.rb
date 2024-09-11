2607e46feca39706dd327749892a7283087f0b67
Christian <cedenoc181@gmail.com>
2024-09-11 00:00:05 -0400

    added jwt token to application controller and more methods to user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,51 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     include ActionController::Cookies
-
+    before_action :authorized
+###################################################
+      #test
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
       end
-      #test
+    
       def yer 
         render json: {Christian: "what up gansta"}
       end
+
+###################################################
+
+      def encode_token(payload)
+        JWT.encode(payload, 'password') 
+    end
+
+    def decoded_token
+        header = request.headers['Authorization']
+        if header
+            token = header.split(" ")[1]
+            begin
+                JWT.decode(token, 'password')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user 
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+    
+    def authorized
+        unless !!current_user
+        render json: { message: 'Please log in' }, status: :unauthorized
+        end
+    end
+
+    def render_unprocessable_entity(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
-  before_action :find_user, only: %i[ show update destroy ]
+  before_action :find_user, only: %i[ show destroy ]
+  skip_before_action :authorized, only: [:create :forgot_password]
+
 
   # GET /users
   def index
@@ -10,31 +12,74 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: user, status: :ok
+    render json: user, serializer: UserSerializer, status: :ok
+  end
+
+  def me 
+    render json: current_user, status: :ok
   end
 
   # POST /users
   def create
     user = User.create!(create_user_params)
-    render json: user
-
+    token = encode_token({user_id: user.id})
+    render json: {user: UserSerializer.new(user), jwt: token}, status: :created
   end
 
-  # PATCH/PUT /users/1
-  def update
+  def forgot_password
+    user = find_user
+  
+    if user.present?
+      new_password = params[:new_password]  
+      if user.update(password: new_password)
+        render json: { message: "Password was updated" }, status: :ok
+      else
+        render json: { message: "Password update failed" }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: "User not found" }, status: :not_found
+    end
+  end
+
+
+
+ # PATCH/PUT /users/:id
+def update
+  # Check if the user is an admin and updating another user
+  if isAdmin?
+    user = find_user
     if user.update(user_editable_params)
       render json: user
     else
       render json: user.errors, status: :unprocessable_entity
     end
+
+  # Check if the current user is updating their own profile
+  elsif current_user.update(user_editable_params)
+    render json: current_user
+
+  # If neither, return errors
+  else
+    render json: current_user.errors, status: :unprocessable_entity
   end
+end
 
   # DELETE /users/1
   def destroy
-    user.destroy!
+    begin
+      user.destroy!
+      render json: { message: "User successfully deleted." }, status: :ok
+    rescue ActiveRecord::RecordNotDestroyed => e
+      render json: { error: "User could not be deleted: #{e.message}" }, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def isAdmin?
+    current_user.admin?
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def find_user
       user = User.find(params[:id])
