113268ee6d210919d167d62913397344bbf71d80
Christian <cedenoc181@gmail.com>
2024-09-10 22:26:33 -0400

    updated user serializer and adding to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,49 +3,46 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    users = User.all
+    render json: users, status: :ok
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user = find_user
+    render json: user, status: :ok
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.create!(create_user_params)
+    render json: user
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
   end
+
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy!
+    user.destroy!
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+    def find_user
+      user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user, {})
+    def create_user_params
+      params.permit()
     end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :email, :role, :clinic_location, :insurance_network, :direct_access, :admin, :created_at 
+  attributes :id, :full_name, :email, :role, :clinic_location, :insurance_network, :direct_access, :admin
+
+  attribute :created_at do |object|
+    object.created_at.strftime("%B %d, %Y")
+  end
+
+  has_many :my_medifiles
+
+  has_many :my_templates
+
 end
