8e2725975498cc2afab35efbbd230536106efde3
Christian <cedenoc181@gmail.com>
2024-09-12 12:31:43 -0400

    rails routes modifications for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
-
     #user path to full crud of these models for nested routes
-  #these models are specifically for users for personalized exp
+  #these models are specifically for users for personalized exp for nom-admins
   resources :users do 
     resources :my_templates
     resources :my_medifiles
   end
 
-  #admin will be only user to create instances of these models, 
   #admin accoubt will be able to do full crud of these templates and files 
+  resources :my_templates, only: [:index, :show, :delete]
+  resources :my_medifiles, only: [:index, :show, :delete]
 
+    #admin will be only user to create instances of these models, 
   resources :dr_templates
   resources :patient_templates
   resources :medifiles
@@ -19,7 +20,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "/up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
@@ -36,10 +37,10 @@ Rails.application.routes.draw do
 ###################################################
 
 #user controller custom actions 
-get "/my-account", to: "user#me"
+get "/my-account", to: "users#me"
 
 #only to be used as link sent to users email address
-patch "/forgot-password", to: "user#forgot_password
+patch "/forgot-password", to: "users#forgot_password"
 
 
 
