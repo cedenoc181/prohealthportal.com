a9bf0faf77124c39a195baf48c4b96ef19a8723d
Christian <cedenoc181@gmail.com>
2024-09-24 15:15:17 -0400

    seeded data and all uploaded to aws, updated controller for medifile and the serializer as well

--- app/controllers/medifiles_controller.rb
+++ app/controllers/medifiles_controller.rb
@@ -1,11 +1,11 @@
 class MedifilesController < ApplicationController
   before_action :set_medifile, only: %i[ show update destroy ]
+  skip_before_action :authorized, only: [:index, :show]
 
   # GET /medifiles
   def index
     @medifiles = Medifile.all
-
-    render json: @medifiles
+    render json: @medifiles, status: :ok
   end
 
   # GET /medifiles/1
@@ -15,8 +15,10 @@ class MedifilesController < ApplicationController
 
   # POST /medifiles
   def create
+    unless is_admin?
+      return render json: { errors: "User is not authorized to create medical file" }, status: :forbidden
+    end
     @medifile = Medifile.new(medifile_params)
-
     if @medifile.save
       render json: @medifile, status: :created, location: @medifile
     else
@@ -26,16 +28,27 @@ class MedifilesController < ApplicationController
 
   # PATCH/PUT /medifiles/1
   def update
+    unless is_admin?
+      return render json: { errors: "User is not authorized to update template" }, status: :forbidden
+    end
     if @medifile.update(medifile_params)
       render json: @medifile
-    else
+    else 
       render json: @medifile.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /medifiles/1
   def destroy
-    @medifile.destroy!
+    unless is_admin?
+      return render json: { errors: "User is not authorized to delete medical file" }, status: :forbidden
+    end
+    if @medifile.destroy
+      render json: { message: "medical file destroyed successfully" }, status: :no_content   
+    else
+      render json: {error: "medical file not found"}
+    end
+    end
   end
 
   private
@@ -46,6 +59,6 @@ class MedifilesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def medifile_params
-      params.fetch(:medifile, {})
+      params.permit(:title, :description, :instructions, :language, :file_editable, :file_cover, :file_cover_alt, :file_link)
     end
 end
--- app/serializers/medifile_serializer.rb
+++ app/serializers/medifile_serializer.rb
@@ -1,3 +1,3 @@
 class MedifileSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :file_link, :file_cover, :file_cover_alt, :publish_date, :language, :file_editable
+  attributes :id, :title, :description, :instructions, :file_link, :file_cover, :file_cover_alt, :language, :file_editable
 end
