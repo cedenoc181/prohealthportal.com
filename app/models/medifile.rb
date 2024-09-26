e146c7a7c445de243f19dafa7c631334f66b5be8
Christian <cedenoc181@gmail.com>
2024-09-25 23:29:08 -0400

    medifile serializer is loading the file links in the form of active storage relative path

--- app/models/medifile.rb
+++ app/models/medifile.rb
@@ -5,13 +5,4 @@ class Medifile < ApplicationRecord
     has_one_attached :file_link
     has_one_attached :file_cover
 
-    private 
-
-    # def aws_medifile_update
-    #     self.update(
-    #         self.file_link = ,
-    #           self.file_cover =
-    #     )
-    # end
-
 end
--- app/serializers/medifile_serializer.rb
+++ app/serializers/medifile_serializer.rb
@@ -1,3 +1,20 @@
 class MedifileSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :instructions, :file_link, :file_cover, :file_cover_alt, :language, :file_editable
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title, :description, :instructions, :file_cover_alt, :language, :file_editable, :file_link_url, :file_cover_url
+
+
+def file_link_url 
+  if object.file_link.attached?
+    rails_blob_url(object.file_link, only_path: true)
+  end
+end
+
+def file_cover_url 
+    if object.file_cover.attached?
+      rails_blob_url(object.file_cover, only_path: true)
+    end
+end
+
+
 end
