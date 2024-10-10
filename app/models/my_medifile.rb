e9194bf2026a720a4a256ef84d457a9fd8d8cacb
Christian <cedenoc181@gmail.com>
2024-10-10 11:04:22 -0400

    made my mediifile shareable with coworkers, by duplicating when ann instance is created and commited, now the corrolating user will have a copy of the instance

--- app/models/my_medifile.rb
+++ app/models/my_medifile.rb
@@ -1,5 +1,26 @@
 class MyMedifile < ApplicationRecord
+
+after_create_commit :duplicate
+
     belongs_to :user
     belongs_to :coworker, class_name: 'User', optional: true
     belongs_to :medifile
+
+
+
+
+private 
+
+def duplicate
+
+    coworkers_copy = self.dup
+    p "#{coworkers_copy} duplicate"
+
+    coworkers_copy.user_id = self.coworker_id
+    coworkers_copy.coworker_id = self.user_id
+
+    coworkers_copy.save
+end 
+
+
 end
