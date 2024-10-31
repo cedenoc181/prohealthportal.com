43abd62e5cd322f296ad7330956a520c8788b6f8
Christian <cedenoc181@gmail.com>
2024-10-31 00:25:37 -0400

    added button to console

--- frontend/src/Components/Dashboard/Features/Medical.jsx
+++ frontend/src/Components/Dashboard/Features/Medical.jsx
@@ -40,7 +40,10 @@ export const Medical = (props) => {
 </div>
       <br />
 <div className="selected-menu">{ defaultLanguage ? "English Forms" : "Spanish Forms"}</div>
-<button className="addTemplate"></button>
+<button className="addTemplate"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="currentColor" class="bi bi-file-plus" viewBox="0 0 16 16">
+  <path d="M8.5 6a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V10a.5.5 0 0 0 1 0V8.5H10a.5.5 0 0 0 0-1H8.5z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1"/>
+</svg></button>
 
           <div className="filter-Search">
             <div className="search-container">  
@@ -86,7 +89,7 @@ export const Medical = (props) => {
                     <br />
                      <div className="medical-category"><span className="key">Category:</span> Authorization</div>
                 </div>
-                
+
                 <div className="renderMedical">
                 <div className="medical-title">Doc title 2</div>
                     <img className="medical-cover" src={""} alt={""}/>
