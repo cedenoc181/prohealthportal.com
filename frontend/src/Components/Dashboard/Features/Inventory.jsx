23f92eaba1c2c39e3d2cd164f83d4b2e8d10978f
Christian <cedenoc181@gmail.com>
2024-11-04 11:06:30 -0500

    adding styling to inventory design

--- frontend/src/Components/Dashboard/Features/Inventory.jsx
+++ frontend/src/Components/Dashboard/Features/Inventory.jsx
@@ -1,16 +1,171 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { connect } from 'react-redux'
-import InventoryMain from './Main/InventoryMain.jsx';
 import "./Features.css"
+import { Input } from '@chakra-ui/react'
 
 export const Inventory = (props) => {
+
+  const [collapse, setCollapse] = useState(false);
+
+  const handleTemplate = () => setCollapse(!collapse);
+
+let count = Math.floor(Math.random() * 10) + 1;
+
+
+
   return (
-    <div id="inventory" className="console">Inventory
-    
-    <div>
-        <InventoryMain />
+    <div id="inventory" className="console"> 
+
+<div className="console-title">Inventory<span>
+   <div className="sideMenuItems">
+   <svg className="svg" onClick={handleTemplate} xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+      <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+   </svg>
+  </div>
+ {collapse ? 
+ ( 
+  // if front desk/ PT or OT 
+  //daily operations will be for:  all task that fd will be in charge of like schedule, inventory, medifiles, checklist 
+  // authorizations will be for : auth status, track D.A, case management(get new Rx),  
+  //APOS will track: pick up status, signature pending, date letter faxed, 1st FU visit scheduled
+  <ul className="filter-li-container">
+      <li className="filter-li">West 180</li> 
+      <li className="filter-li">West 150</li>
+      <li className="filter-li">Eastside</li>
+  </ul>
+
+)
+ :
+  (
+      ""
+  )
+}
+</span>
+</div>
+{/* <br /> */}
+<div className="selected-menu">West 180</div>
+
+<div className="inventory-con">
+<h2 className="low-inv-title">Insufficient</h2>
+<ul className="low-inv-list">
+  <li className="low-item">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+  <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+  <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+</svg>
+business cards :
+<span className="inv-cur-count">{count}</span>
+available
+  </li>
+  <li className="low-item">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+  <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+  <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+</svg>
+  electrodes :
+  <span className="inv-cur-count">{count}</span>
+  available
+  </li>
+  <li className="low-item">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+  <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+  <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+</svg>
+printing paper :
+<span className="inv-cur-count">{count}</span>
+available
+  </li>
+  <li className="low-item">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+  <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+  <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+</svg>
+paper towels :
+<span className="inv-cur-count">{count}</span>
+available
+  </li>
+</ul>
+ <div className="inv-expand">
+    <button>Show more...</button>
     </div>
-    
+</div>
+<br />
+
+<div className="inventory-con">
+<h2 className="inv-order-title">Order status</h2>
+  <ul className="inv-order-list">
+    <li className="ordered-item">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7"/>
+  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+</svg>
+(PENDING)
+<span className="item-name-quant"> item name: {count}</span>
+    </li>
+    <li className="ordered-item">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+  <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
+</svg>
+(ORDERED)
+<span className="item-name-quant"> item name: {count}</span>
+    </li>
+    <li className="ordered-item">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+  <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+</svg>
+(DELIVERED)
+<span className="item-name-quant"> item name: {count}</span>
+    </li>
+    <li className="ordered-item">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+  <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
+</svg>
+(ORDERED)
+<span className="item-name-quant"> item name: {count}</span>
+    </li>
+   </ul>
+   <div className="inv-expand">
+    <button>Show more...</button>
+    </div>
+</div>
+<br />
+
+<div className="inventory-req">
+<h2 className="inv-req-title">Request form</h2>
+<form className="inv-form">
+
+<Input className="inv-input"  placeholder='Item' />
+
+<Input className="inv-input"  placeholder='Item link' />
+
+    <div class="input-group mb-3 inv-menu">
+  <label class="input-group-text" for="inputGroupSelect01">Category</label>
+  <select class="form-select" id="inputGroupSelect01">
+    <option selected>Choose...</option>
+    <option value="1">Office</option>
+    <option value="2">Cleaning</option>
+    <option value="3">Equipment</option>
+  </select>
+</div>
+
+    <div class="input-group mb-3 inv-menu">
+  <label class="input-group-text" for="inputGroupSelect01">Count per Box</label>
+  <select class="form-select" id="inputGroupSelect01">
+    <option selected>Count...</option>
+    <option value="1">1-3</option>
+    <option value="2">4-6</option>
+    <option value="3">7-10</option>
+  </select>
+</div>
+
+    <button className="inv-submission">Request</button> {/* submit */}
+</form>
+</div>
+
+
+
+
     </div>
   )
 }
