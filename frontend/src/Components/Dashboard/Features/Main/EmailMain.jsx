0945784597d46d1b65e92df860673f1ad9b29f4c
Christian <cedenoc181@gmail.com>
2025-01-02 23:43:27 -0500

    chaning email template to only send

--- frontend/src/Components/Dashboard/Features/Main/EmailMain.jsx
+++ frontend/src/Components/Dashboard/Features/Main/EmailMain.jsx
@@ -289,56 +289,9 @@ setTimeout(() => {
                 
               }
           </Button>
-          <Button colorScheme='blue'  height='48px' width='200px' onClick={handleSaveTemplateDropMenu}>
-            {
-              saveTemplateDropMenu ? 
-              "Close"
-              :
-              "Save Template"
-            }
-        
-        </Button>
       </ButtonGroup>
       </div>
       <br />
-      {
-        saveTemplateDropMenu ? 
-        (
-        <form className="save-email-form">
-          <h2 className="Save-email-title">Add to your list of Template</h2>
-          <br />
-            <div className="email-notes"> 
-                <label mb='8px'>Notes:</label>
-                <Textarea
-                className="email-textarea"
-                   placeholder='Note email info to optimize for patient or Dr. interaction, keep track and organized.'
-                   size='md'
-                   name="notes"  // This is necessary to ensure the correct property is updated in your state
-                   value={myPatientTemplate.notes}
-                  onChange={handlePatientTemplateChange}
-                  required
-                />
-            </div>
-            <br />
-            <div className="email-submit-button">
-            <Button  colorScheme='blue' variant='solid' size='lg'>
-                Save <span className="downloadIcon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
-                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
-                </svg>
-                </span>
-            </Button>
-            </div>
-        </form>
-        ) 
-        : 
-
-        (
-        <div className="empty-menu">
-        </div>
-        )
-      }
 </div>
           
       <div className="export-emails">
