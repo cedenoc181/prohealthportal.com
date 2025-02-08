d82bd7d09e731c4497631b5b5c9909037f490d75
Christian <cedenoc181@gmail.com>
2025-02-08 17:50:07 -0500

    requested items rendering as designed

--- app/serializers/requested_item_serializer.rb
+++ app/serializers/requested_item_serializer.rb
@@ -1,9 +1,13 @@
 class RequestedItemSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :clinic_id, :item_type, :item_name, :item_link, :count, :created_at, :updated_at
+  attributes :id, :item_type, :item_name, :item_link, :count, :ordered, :created_at, :updated_at, :requested_by, :to_clinic
 
-  private 
 
-  def clinic_orders
-    object.clinics.group_by(&:clinic_id)
-  end
+def requested_by
+  object.user&.slice(:first_name, :last_name)
+end 
+
+def to_clinic
+  object.clinic&.slice(:clinic_location_name, :clinic_location_address)
+end 
+
 end
