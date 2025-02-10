8e7f62eeff0da183f1d831c3c17a7bfa2e581655
Christian <cedenoc181@gmail.com>
2025-02-10 10:02:57 -0500

    created a new method in models that created requested_item instance if the inventory requested_item turns true based on a modification of a inventory item

--- app/models/inventory_item.rb
+++ app/models/inventory_item.rb
@@ -1,10 +1,26 @@
 class InventoryItem < ApplicationRecord
     belongs_to :clinic, inverse_of: :inventory_items  
     belongs_to :user 
+    has_many :requested_items, through: :clinic
 
     around_save :item_status_update
+    after_save :request_insufficient_item
 
     private
+
+    def request_insufficient_item
+        if self.saved_change_to_item_requested?(from: false, to: true)
+            self.clinic.requested_items.create!(
+                clinic_id: self.clinic_id, 
+                item_name: self.item_name, 
+                item_type: self.item_type,
+                item_link: self.item_link,
+                count: self.warning_count * 2,
+                user_id: self.user_id,
+                ordered: false
+            )
+        end
+    end
 
     def item_status_update 
             self.item_requested = count < warning_count
--- app/models/requested_item.rb
+++ app/models/requested_item.rb
@@ -1,5 +1,6 @@
 class RequestedItem < ApplicationRecord
     belongs_to :clinic, inverse_of: :requested_items 
     belongs_to :user
+    
 
 end
